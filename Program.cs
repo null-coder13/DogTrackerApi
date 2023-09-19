0fb4e9f3eba973a14674a5684ebe645b559024cf
Tom <tgadacy@gmail.com>
2023-09-18 23:07:27 -0400

    Update cors, remove weather

--- Program.cs
+++ Program.cs
@@ -4,15 +4,24 @@ using DogTrackerApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("mypolicy", builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+            });
+        });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<DogTrackerContext>(opt =>
-    opt.UseInMemoryDatabase("TodoList"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DatabaseConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +29,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("mypolicy");
 
 app.UseHttpsRedirection();
 
